b840916c78f9e6370820168902ab6f48e10455c6
Michael Han <michael_hll@hotmail.com>
2024-03-26 08:15:49 +0800

    support to process a directory

--- autosubtitle.py
+++ autosubtitle.py
@@ -1,133 +1,199 @@
 import os
+import sys
 import whisper
 import argparse
 import warnings
 import shutil
 from utils import filename, str2bool, write_srt, run_ffmpeg_command
 from csrt import translateSrt
+import uuid
+import glob
+from tqdm import tqdm
+
+temp_dir = os.path.join(os.getcwd(), "temp")
+
 
 def main():
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("video", nargs="+", type=str,
-                        help="paths to video files to transcribe")
-    parser.add_argument("--model", default="small",
-                        choices=whisper.available_models(), help="name of the Whisper model to use")
-    parser.add_argument("--output_dir", "-o", type=str,
-                        default="./out", help="directory to save the outputs")
-    parser.add_argument("--output_srt", type=str2bool, default=False,
-                        help="whether to output the .srt file along with the video files")
-    parser.add_argument("--srt_only", type=str2bool, default=False,
-                        help="only generate the .srt file and not create overlayed video")
-    parser.add_argument("--verbose", type=str2bool, default=False,
-                        help="whether to print out the progress and debug messages")
-    parser.add_argument("--task", type=str, default="transcribe", choices=[
-                        "transcribe", "translate"], help="whether to perform X->X speech recognition ('transcribe') or X->English translation ('translate')")
-    parser.add_argument("--language", type=str, default="auto", choices=["auto","af","am","ar","as","az","ba","be","bg","bn","bo","br","bs","ca","cs","cy","da","de","el","en","es","et","eu","fa","fi","fo","fr","gl","gu","ha","haw","he","hi","hr","ht","hu","hy","id","is","it","ja","jw","ka","kk","km","kn","ko","la","lb","ln","lo","lt","lv","mg","mi","mk","ml","mn","mr","ms","mt","my","ne","nl","nn","no","oc","pa","pl","ps","pt","ro","ru","sa","sd","si","sk","sl","sn","so","sq","sr","su","sv","sw","ta","te","tg","th","tk","tl","tr","tt","uk","ur","uz","vi","yi","yo","zh"], 
-                        help="What is the origin language of the video? If unset, it is detected automatically.")
-    parser.add_argument("--gemini_model", default="",
-                        choices=["gemini-pro", "gemini"], help="name of the gemini model")
-    parser.add_argument("--language_to", type=str, default="zh", choices=["af","am","ar","as","az","ba","be","bg","bn","bo","br","bs","ca","cs","cy","da","de","el","en","es","et","eu","fa","fi","fo","fr","gl","gu","ha","haw","he","hi","hr","ht","hu","hy","id","is","it","ja","jw","ka","kk","km","kn","ko","la","lb","ln","lo","lt","lv","mg","mi","mk","ml","mn","mr","ms","mt","my","ne","nl","nn","no","oc","pa","pl","ps","pt","ro","ru","sa","sd","si","sk","sl","sn","so","sq","sr","su","sv","sw","ta","te","tg","th","tk","tl","tr","tt","uk","ur","uz","vi","yi","yo","zh"], 
-                        help="The language code to translate the subtitle to using gemini_model")
+  parser = argparse.ArgumentParser(
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument("video", nargs="*", type=str,
+                      default=None, help="paths to video files to transcribe")
+  parser.add_argument("--input_dir", "-i", type=str, nargs='*',
+                      default=None, help="directory of mp4 files")
+  parser.add_argument("--model", default="small",
+                      choices=whisper.available_models(), help="name of the Whisper model to use")
+  parser.add_argument("--output_dir", "-o", type=str,
+                      default="", help="directory to save the outputs")
+  parser.add_argument("--srt_only", type=str2bool, default=False,
+                      help="only generate the .srt file and not create overlayed video")
+  parser.add_argument("--verbose", type=str2bool, default=False,
+                      help="whether to print out the progress and debug messages")
+  parser.add_argument("--task", type=str, default="transcribe", choices=[
+                      "transcribe", "translate"], help="whether to perform X->X speech recognition ('transcribe') or X->English translation ('translate')")
+  parser.add_argument("--language", type=str, default="auto", choices=["auto", "af", "am", "ar", "as", "az", "ba", "be", "bg", "bn", "bo", "br", "bs", "ca", "cs", "cy", "da", "de", "el", "en", "es", "et", "eu", "fa", "fi", "fo", "fr", "gl", "gu", "ha", "haw", "he", "hi", "hr", "ht", "hu", "hy", "id", "is", "it", "ja", "jw", "ka", "kk", "km", "kn", "ko", "la", "lb", "ln", "lo", "lt", "lv", "mg", "mi", "mk", "ml", "mn", "mr", "ms", "mt", "my", "ne", "nl", "nn", "no", "oc", "pa", "pl", "ps", "pt", "ro", "ru", "sa", "sd", "si", "sk", "sl", "sn", "so", "sq", "sr", "su", "sv", "sw", "ta", "te", "tg", "th", "tk", "tl", "tr", "tt", "uk", "ur", "uz", "vi", "yi", "yo", "zh"],
+                      help="What is the origin language of the video? If unset, it is detected automatically.")
+  parser.add_argument("--gemini_model", default="",
+                      choices=["gemini-pro", "gemini"], help="name of the gemini model")
+  parser.add_argument("--language_to", type=str, default="zh", choices=["af", "am", "ar", "as", "az", "ba", "be", "bg", "bn", "bo", "br", "bs", "ca", "cs", "cy", "da", "de", "el", "en", "es", "et", "eu", "fa", "fi", "fo", "fr", "gl", "gu", "ha", "haw", "he", "hi", "hr", "ht", "hu", "hy", "id", "is", "it", "ja", "jw", "ka", "kk", "km", "kn", "ko", "la", "lb", "ln", "lo", "lt", "lv", "mg", "mi", "mk", "ml", "mn", "mr", "ms", "mt", "my", "ne", "nl", "nn", "no", "oc", "pa", "pl", "ps", "pt", "ro", "ru", "sa", "sd", "si", "sk", "sl", "sn", "so", "sq", "sr", "su", "sv", "sw", "ta", "te", "tg", "th", "tk", "tl", "tr", "tt", "uk", "ur", "uz", "vi", "yi", "yo", "zh"],
+                      help="The language code to translate the subtitle to using gemini_model")
 
-    args = parser.parse_args().__dict__
-    model_name: str = args.pop("model")
-    output_dir: str = args.pop("output_dir")
-    output_srt: bool = args.pop("output_srt")
-    srt_only: bool = args.pop("srt_only")    
-    language: str = args.pop("language")
-    language_to: str = args.pop("language_to")
-    
-    gemini_model: str = args.pop("gemini_model")
-    verbose: bool = args["verbose"]
-    
-    shutil.rmtree(output_dir, ignore_errors=True, onerror=None)
-    os.makedirs(output_dir, exist_ok=True)
+  args = parser.parse_args().__dict__
+  model_name: str = args.pop("model")
+  input_dir: str = args.pop("input_dir", None)
+  video: str = args.pop("video", None)
+  output_dir: str = args.pop("output_dir")
+  srt_only: bool = args.pop("srt_only")
+  language: str = args.pop("language")
+  language_to: str = args.pop("language_to")
 
-    if model_name.endswith(".en"):
-        warnings.warn(
-            f"{model_name} is an English-only model, forcing English detection.")
-        args["language"] = "en"
-    # if translate task used and language argument is set, then use it
-    elif language != "auto":
-        args["language"] = language
-        
-    model = whisper.load_model(model_name)
-    
-    # get audio file
-    audios = get_audio(args.pop("video"), verbose, output_dir)
+  gemini_model: str = args.pop("gemini_model")
+  verbose: bool = args["verbose"]
 
-    # get sub_titles
-    subtitles = get_subtitles(
-        audios, 
-        output_srt or srt_only, 
-        output_dir, 
+  # get all .mp4 files need to be processed
+  to_processed_files = []
+  processed_result = {}
+  if video:
+    if video[0] and not video[0].endswith('.mp4'):
+      raise Exception('Only mp4 files can be processed.')
+    to_processed_files.append(video[0])
+  elif input_dir:
+    try:
+      input_d = input_dir[0] + "/*.mp4"
+      to_processed_files = glob.glob(input_d)
+    except Exception as e:
+      print(e)
+      sys.exit()
+  else:
+    print('No input video files need to be processed!')
+    sys.exit()
+
+  if len(to_processed_files) == 0:
+    raise Exception("There is no input mp4 videos found.")
+  print('==> processed files:', to_processed_files)
+
+  # check output dir and temp dir
+  if not output_dir:
+    output_dir = os.path.join(os.getcwd(), 'out')
+  os.makedirs(output_dir, exist_ok=True)
+  shutil.rmtree(temp_dir, ignore_errors=True, onerror=None)
+  os.makedirs(temp_dir, exist_ok=True)
+
+  # Loading the whisper model
+  if model_name.endswith(".en"):
+    warnings.warn(
+        f"{model_name} is an English-only model, forcing English detection.")
+    args["language"] = "en"
+  # if translate task used and language argument is set, then use it
+  elif language != "auto":
+    args["language"] = language
+
+  model = whisper.load_model(model_name)
+
+  # gothrough each mp4 file
+  for video in tqdm(to_processed_files, '==> TOTAL PROGRESS: '):
+
+    # copy it to temp folder
+    temp_file_id = str(uuid.uuid1())
+    temp_video_name = os.path.join(temp_dir, temp_file_id + '.mp4')
+    processed_result[video] = {
+        "uuid": "",  # the uuid used to the following temp file names
+        "temp": "",  # temp video name
+        "aac": "",  # temp aac audio file name
+        "srt": "",  # temp srt file name
+        "srt_t": "",  # temp srt translation file name
+    }
+    processed_result[video]['uuid'] = temp_file_id
+    processed_result[video]["temp"] = temp_video_name
+    shutil.copy(video, temp_video_name)
+
+    # get audio file (aac file)
+    get_audio(video, processed_result, verbose)
+
+    # get sub_titles (srt file)
+    get_subtitles(
+        video,
+        processed_result,
         lambda audio_path: model.transcribe(audio_path, **args)
     )
-    
-    # translate subtitles   
-    if gemini_model: 
+
+    # translate subtitles
+    if gemini_model:
       # using google gemini to translate the output srt file
-      for path, srt_path in subtitles.items():
-        print(f"Translating subtitles for {filename(path)}... This might take a while.")
-        srt_t_path = os.path.join(output_dir, f"{filename(path)}_t.srt")     
-        errors = [] 
-        translateSrt(srt_path, language_to, srt_t_path, gemini_model, verbose, errors)  
-        print('==> error item nubmers:', errors)  
-    
+      print(
+          f"Translating subtitles for '{filename(video)}.mp4' This might take a while.")
+      srt_t_temp_file_name = processed_result[video]['uuid'] + '_t.srt'
+      srt_t_path = os.path.join(temp_dir, srt_t_temp_file_name)
+      errors = []
+      translateSrt(processed_result[video]['srt'], language_to, srt_t_path,
+                   gemini_model, verbose, errors)
+      processed_result[video]['srt_t'] = srt_t_path
+      print('==> error item nubmers:', errors)
+
     # auto add subtitles to the original video
     # using ffmpeg command to achieve this
     if not srt_only:
-      for path, srt_path in subtitles.items():
-        srt_t_path = os.path.join(output_dir, f"{filename(path)}_t.srt") 
-        out_path = os.path.join(output_dir, f"{filename(path)}_out.mp4")
+      temp_video_out_name = processed_result[video]['uuid'] + '.out.mp4'
+      temp_video_out_path = os.path.join(temp_dir, temp_video_out_name)
+      processed_result[video]['out'] = temp_video_out_path
+      srt_t_path = processed_result[video]['srt_t']
 
-        print(f"Adding subtitles to {filename(path)}...")
-        
-        if not gemini_model:
-          # only add one subtitle to the video, using the whisper
-          run_ffmpeg_command(
-            f"ffmpeg -y -i {path} -i {srt_path} -map 0:v -map 0:a -map 1 -c:v copy -c:a copy -c:s mov_text -metadata:s:s:0 language={language} {out_path}",
+      print(f"Adding subtitles to {filename(video)}...")
+
+      if not gemini_model:
+        # only add one subtitle to the video, using the whisper
+        run_ffmpeg_command(
+            f"ffmpeg -y -i {processed_result[video]['temp']} -i {processed_result[video]['srt']} -map 0:v -map 0:a -map 1 -c:v copy -c:a copy -c:s mov_text -metadata:s:s:0 language={language} {temp_video_out_path}",
             verbose)
-        else:
-          # translate then add both subtitles, using google
-          run_ffmpeg_command(
-            f"ffmpeg -y -i {path} -i {srt_path} -i {srt_t_path} -map 0:v -map 0:a -map 1 -map 2 -c:v copy -c:a copy -c:s mov_text -metadata:s:s:0 language={language} -metadata:s:s:1 language={language_to} {out_path}",
+      else:
+        # translate then add both subtitles, using google
+        run_ffmpeg_command(
+            f"ffmpeg -y -i {processed_result[video]['temp']} -i {processed_result[video]['srt']} -i {srt_t_path} -map 0:v -map 0:a -map 1 -map 2 -c:v copy -c:a copy -c:s mov_text -metadata:s:s:0 language={language} -metadata:s:s:1 language={language_to} {temp_video_out_path}",
             verbose)
 
-        print(f"Saved subtitled video to {os.path.abspath(out_path)}.")     
-    
-def get_audio(paths, verbose, output_dir):
-    audio_paths = {}
+    # copy temp files to the output folder
+    final_output = os.path.join(
+        output_dir, filename(video) + '.mp4')
+    shutil.copy(temp_video_out_path, final_output)
+    if processed_result[video]['srt']:
+      shutil.copy(processed_result[video]['srt'], os.path.join(
+          output_dir, filename(video) + '.srt'))
+    if processed_result[video]['srt_t']:
+      shutil.copy(processed_result[video]['srt_t'], os.path.join(
+          output_dir, filename(video) + '_t.srt'))
+    print(f"Saved subtitled video to {os.path.abspath(final_output)}.")
 
-    for path in paths:
-        print(f"Extracting audio from {filename(path)}...")
-        output_path = os.path.join(output_dir, f"{filename(path)}.aac")
 
-        run_ffmpeg_command("ffmpeg -y -i " + path + " -vn -acodec copy " + output_path, verbose)
+def get_audio(video, processed_result, verbose):
 
-        audio_paths[path] = output_path
+  temp_video_file = processed_result[video]["temp"]
+  temp_audio_file_out = processed_result[video]["uuid"] + ".aac"
+  print(f"Extracting audio from {filename(video)}...")
+  audio_output_path = os.path.join(temp_dir, temp_audio_file_out)
 
-    return audio_paths
+  run_ffmpeg_command("ffmpeg -y -i " + temp_video_file +
+                     " -vn -acodec copy " + audio_output_path, verbose)
 
-def get_subtitles(audio_paths: list, output_srt: bool, output_dir: str, transcribe: callable):
-    subtitles_path = {}
+  processed_result[video]["aac"] = audio_output_path
 
-    for path, audio_path in audio_paths.items():
-        srt_path = os.path.join(output_dir, f"{filename(path)}.srt")
-        
-        print(f"Generating subtitles for {filename(path)}... This might take a while.")
 
-        warnings.filterwarnings("ignore")
-        result = transcribe(audio_path)
-        warnings.filterwarnings("default")
+def get_subtitles(video, processed_result, transcribe: callable):
 
-        with open(srt_path, "w", encoding="utf-8") as srt:
-          write_srt(result["segments"], file=srt)
+  temp_aac_file = processed_result[video]['aac']
+  srt_temp_file = processed_result[video]['uuid'] + '.srt'
+  srt_file_temp_path = os.path.join(temp_dir, srt_temp_file)
 
-        subtitles_path[path] = srt_path
+  print(
+      f"Generating subtitles for '{filename(video)}.mp4' This might take a while.")
 
-    return subtitles_path
+  warnings.filterwarnings("ignore")
+  result = transcribe(temp_aac_file)
+  warnings.filterwarnings("default")
+
+  with open(srt_file_temp_path, "w", encoding="utf-8") as srt:
+    write_srt(result["segments"], file=srt)
+
+  processed_result[video]['srt'] = srt_file_temp_path
+
 
 if __name__ == '__main__':
-    main()
+  main()
--- csrt.py
+++ csrt.py
@@ -9,31 +9,34 @@ load_dotenv()  # take environment variables from .env.
 
 genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
 
-def translateSrt(srtFilePath, language, outPath, model_name, verbose = False, errors = []):
+
+def translateSrt(srtFilePath, language, outPath, model_name, verbose=False, errors=[]):
   transCommand = "please translate this sentence to {0}: {1}"
   with open(srtFilePath) as f:
-      lines = f.readlines()
-      with open(outPath, 'w') as wf:
-        lineNo = 0
-        for line in tqdm(lines):
-          line = line.replace('\n', '')          
-          if line.isnumeric():
-            lineNo = int(line)
-          if not (line.isnumeric() or '-->' in line) and line:       
-            lineBackup = line     
-            line = __askGemini(transCommand.format(language, line), model_name, verbose)
-            if not line:
-              errors.append(lineNo)
-              line = '[TRANSATION-FAIL]: ' + lineBackup
-            if verbose:
-              print(line)
-          wf.write(line + '\n')
-          
+    lines = f.readlines()
+    with open(outPath, 'w') as wf:
+      lineNo = 0
+      for line in tqdm(lines, '==> translation file: '):
+        line = line.replace('\n', '')
+        if line.isnumeric():
+          lineNo = int(line)
+        if not (line.isnumeric() or '-->' in line) and line:
+          lineBackup = line
+          line = __askGemini(transCommand.format(
+              language, line), model_name, verbose)
+          if not line:
+            errors.append(lineNo)
+            line = '[TRANSATION-FAIL]: ' + lineBackup
+          if verbose:
+            print(line)
+        wf.write(line + '\n')
+
+
 def __askGemini(input, model_name, verbose=False):
   response = None
-  try:    
-    model = genai.GenerativeModel(model_name)  
-    response = model.generate_content(input)   
+  try:
+    model = genai.GenerativeModel(model_name)
+    response = model.generate_content(input)
     # sleep 1 seconds inorder not reach the upper limit
     time.sleep(1)
     return response.text
@@ -41,7 +44,3 @@ def __askGemini(input, model_name, verbose=False):
     if verbose:
       print(e, response)
     return ''
-    
-  
-          
- 
